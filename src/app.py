48dd4317c8cc96a3d68c16450042b5654ac0708c
Puupa <arthur.van.acker@gmail.com>
2024-03-01 19:46:29 +0000

    categorical histograms

--- src/app.py
+++ src/app.py
@@ -7,7 +7,8 @@ def main():
 
     df = pd.read_sql("ml_4geeks_airbnb_ny_2019", engine)
 
-    print(df.head())
+    df_clean = df[df.availability_365 > 0]
+    print(df_clean.head())
 
 
 if __name__ == "__main__":
